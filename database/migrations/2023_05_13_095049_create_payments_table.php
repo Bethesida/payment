4019e494b00c896b744e0c558579498bd204aa3c
wealthydeveloper <idongesitutong.com>
2023-05-13 11:02:23 +0100

    first commit

---
+++ database/migrations/2023_05_13_095049_create_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('card_number', 19);
+            $table->integer('expiry_month');
+            $table->integer('expiry_year');
+            $table->string('cvv', 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
